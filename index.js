1281470127f8726a4b6315322246e02eb1a0f8aa
DanielPereiraOFC <fdemasi43@hotmail.com>
2026-02-14 21:36:17 -0300

    niveis de personagem

---
+++ index.js
@@ -0,0 +1,38 @@
+let nomeHeroi = "Daniel"
+let xp = 7000
+let nivel = "Bronze"
+
+switch (true) {
+    case xp < 1000:
+        nivel = "Ferro";
+        break;
+    case xp >= 1000 && xp <= 2000:
+        nivel = "Bronze";
+        break;
+
+        case xp >= 2001 && xp <= 3000:
+        nivel = "Prata";
+        break;
+
+        case xp >= 3001 && xp <= 4000:
+        nivel = "Ouro";
+        break;
+
+        case xp >= 4001 && xp <= 5000:
+        nivel = "Platina";
+        break;
+
+        case xp >= 5001 && xp <= 6000:
+        nivel = "Ascendente";
+        break;
+
+        case xp >= 6001 && xp <= 7000:
+        nivel = "Imortal";
+        break;
+
+        case xp >= 7001 && xp >= 8000:
+        nivel = "Radiante";
+        break;
+        }
+
+console.log("O herói de nome " + nomeHeroi + " está no nível " + nivel);
